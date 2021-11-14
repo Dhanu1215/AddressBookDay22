8723003c0a15116f099ef5a4baa386acfe1f175b
Asus <dhanashreehakke7@gmail.com>
2021-11-14 14:59:12 +0530

    Added: Welcome to Address book program message is added

---
+++ src/main/java/com/blp/addressbook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.blp.addressbook;
+
+/**
+ * Address Book System
+ */
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book program");
+    }
+}
